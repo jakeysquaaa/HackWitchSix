adbad126f0bf41a62cdf7fc0a0ad4f072031e5ff
CM Student <cmstudent@uhwo-acm-11.local>
2020-03-03 10:40:59 -1000

    Hackwitchsix 1-10 and problem set completed.

--- HackWitchSix/ViewController.swift
+++ HackWitchSix/ViewController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var myFavoritecities = ["bali","itally","greece","cancun"]
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFavoritecities.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = myFavoritecities[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+
+    }
+    
+
+
+
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +41,6 @@ class ViewController: UIViewController {
     }
 
 
-}
 
+
+}
